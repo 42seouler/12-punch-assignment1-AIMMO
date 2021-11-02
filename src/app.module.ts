c300bae123b97d8f5ace2a05d04ff5d27c5febea
42seouler <77034008+42seouler@users.noreply.github.com>
2021-11-02 17:33:22 +0900

    add : post-RUD

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { PostsModule } from './posts/posts.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    UsersModule,
+    AuthModule,
+    MongooseModule.forRoot('mongodb://localhost:27017/nest-course'),
+    PostsModule,
+  ],
 })
 export class AppModule {}
--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,16 +1,47 @@
-import { Body, Controller, Post, Req, Request, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete, Get,
+  Param,
+  Patch,
+  Post, Query,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 import CreatePostDto from './dto/create-post.dto';
 import { PostsService } from './posts.service';
+import { UpdatePostDto } from './dto/update-post.dto';
+import { PaginationQueryDto } from '../common/dto/pagination-query.dto';
 
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
+
+  @Get()
+  findAll(@Query() paginationQuery: PaginationQueryDto) {
+    return this.postsService.findAll(paginationQuery);
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.postsService.findOne(id);
+  }
 
   @Post()
   @UseGuards(JwtAuthGuard)
   async createPost(@Body() createPostDto: CreatePostDto, @Request() req) {
     console.log(req.user);
     return this.postsService.create(createPostDto, req.user);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updatePostDto: UpdatePostDto) {
+    return this.postsService.update(id, updatePostDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.postsService.remove(id);
   }
 }
